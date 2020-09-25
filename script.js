26d3668286e49e8bd10792c7e9d8501c74b20bb6
Yuyi50 <yuyisantiago50@gmail.com>
2020-09-25 18:49:32 -0500

    Mis Artistas Favoritos

---
+++ script.js
@@ -0,0 +1,51 @@
+function leon() {
+    let name = prompt("¿Quién es el?")
+    let nombre = document.getElementById (nombre1)
+    nombre1.textContent = name
+
+    let gener = prompt("¿Qué genero interpreta?")
+    let genero = document.getElementById (genero1)
+    genero1.textContent = gener
+
+    let country = prompt("¿De donde es?")
+    let pais = document.getElementById (pais1)
+    pais1.textContent = country
+
+    let song = prompt("Escribe una cancion de el")
+    let cancion = document.getElementById (cancion1)
+    cancion1.textContent = song
+} 
+function roberto() {
+    let name = prompt("¿Quién es el?")
+    let nombre = document.getElementById (nombre2)
+    nombre2.textContent = name
+
+    let gener = prompt("¿Qué genero interpreta?")
+    let genero = document.getElementById (genero2)
+    genero2.textContent = gener
+
+    let country = prompt("¿De donde es?")
+    let pais = document.getElementById (pais2)
+    pais2.textContent = country
+
+    let song = prompt("Escribe una cancion de el")
+    let cancion = document.getElementById (cancion2)
+    cancion2.textContent = song
+}
+function natalia() {
+    let name = prompt("¿Quién es ella?")
+    let nombre = document.getElementById (nombre3)
+    nombre3.textContent = name
+
+    let gener = prompt("¿Qué genero interpreta?")
+    let genero = document.getElementById (genero3)
+    genero3.textContent = gener
+
+    let country = prompt("¿De donde es?")
+    let pais = document.getElementById (pais3)
+    pais3.textContent = country
+
+    let song = prompt("Escribe una cancion de ella")
+    let cancion = document.getElementById (cancion3)
+    cancion3.textContent = song
+} 
